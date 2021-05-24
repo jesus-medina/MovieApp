c5fd615154479b2db5d30bd70b728e3a6e50700d
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2021-05-24 16:37:52 -0700

    GetMostPopularMoviesUseCaseImpl implemented

--- android/app/src/main/java/com/backbase/assignment/domain/usecase/GetMostPopularMoviesUseCase.kt
+++ android/app/src/main/java/com/backbase/assignment/domain/usecase/GetMostPopularMoviesUseCase.kt
@@ -1,6 +1,7 @@
 package com.backbase.assignment.domain.usecase
 
 import com.backbase.assignment.domain.DomainMovie
+import com.backbase.assignment.domain.repository.MovieRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
@@ -8,8 +9,9 @@ interface GetMostPopularMoviesUseCase {
     operator fun invoke(): Flow<List<DomainMovie.DomainMostPopularMovie>>
 }
 
-class GetMostPopularMoviesUseCaseImpl @Inject constructor() : GetMostPopularMoviesUseCase {
-    override fun invoke(): Flow<List<DomainMovie.DomainMostPopularMovie>> {
-        TODO("Not yet implemented")
-    }
+class GetMostPopularMoviesUseCaseImpl @Inject constructor(
+    private val movieRepository: MovieRepository
+) : GetMostPopularMoviesUseCase {
+    override fun invoke(): Flow<List<DomainMovie.DomainMostPopularMovie>> =
+        movieRepository.getMostPopularMovies()
 }
