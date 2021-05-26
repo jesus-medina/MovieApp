57af5c97ffa2350b73d7caef43d35d7a780bff1f
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2021-05-25 22:09:02 -0700

    DomainMostPopularMovieToUIMostPopularMovieMapperImpl implemented

--- android/app/src/main/java/com/backbase/assignment/presentation/mapper/DomainMostPopularMovieToUIMostPopularMovieMapper.kt
+++ android/app/src/main/java/com/backbase/assignment/presentation/mapper/DomainMostPopularMovieToUIMostPopularMovieMapper.kt
@@ -8,8 +8,25 @@ import javax.inject.Inject
 interface DomainMostPopularMovieToUIMostPopularMovieMapper :
     Mapper<DomainMovie.DomainMostPopularMovie, UIMovie.UIMostPopularMovie>
 
-class DomainMostPopularMovieToUIMostPopularMovieMapperImpl @Inject constructor() : DomainMostPopularMovieToUIMostPopularMovieMapper {
-    override fun map(input: DomainMovie.DomainMostPopularMovie): UIMovie.UIMostPopularMovie {
-        TODO("Not yet implemented")
+class DomainMostPopularMovieToUIMostPopularMovieMapperImpl @Inject constructor() :
+    DomainMostPopularMovieToUIMostPopularMovieMapper {
+    override fun map(input: DomainMovie.DomainMostPopularMovie): UIMovie.UIMostPopularMovie =
+        with(input) {
+            UIMovie.UIMostPopularMovie(
+                id,
+                posterImage,
+                title,
+                rating,
+                duration.formatDurationInMinutes(),
+                releaseDate
+            )
+        }
+
+    private fun Int.formatDurationInMinutes(): String {
+        val hours: Int = this / 60
+        val minutes: Int = this % 60
+
+        return (if (hours == 0) "" else " ${hours}h") +
+                if (minutes == 0) "" else " ${minutes}m"
     }
 }
